65bbb697ff020faad5b68f9b53cbf3d3179994f6
Joseph Billings <josephbillings.gb@gmail.com>
2024-07-09 13:26:24 -0600

    created Animal class with greet and feed methods

--- animal-shelter.js
+++ animal-shelter.js
@@ -1,1 +1,16 @@
-const animalData = require('./animal-data.json');
+const animalData = require("./animal-data.json");
+class Animal {
+  constructor(name, species, color, hunger) {
+    this.name = name;
+    this.species = species;
+    this.color = color;
+    this.hunger = hunger;
+  }
+  greet() {
+    console.log(`Hi my name is ${this.name} the ${this.species}!`);
+  }
+  feed() {
+    this.hunger -= 20;
+    console.log("Yum, I love food!");
+  }
+}
